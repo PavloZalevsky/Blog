49099202fdde5c7adb2ee37575dac05ca4ee18fd
PavloZalevsky <pavlo.zalevsky@gmail.com>
2016-04-21 23:44:12 +0300

    controler

---
+++ JustBlog/JustBlog/Models/ListViewModel.cs
@@ -0,0 +1,12 @@
+﻿using JustBlog.Core;
+using JustBlog.Core.Objects;
+using System.Collections.Generic;
+
+namespace JustBlog.Models
+{
+    public class ListViewModel
+    {
+        public IList<Post> Posts { get;  set; }
+        public int TotalPosts { get;  set; }
+    }
+}
